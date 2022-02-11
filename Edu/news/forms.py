5634491e9e2fdd773691d07532c43340d09187ae
LORD-GL <boikogv06@gmail.com>
2022-02-11 14:16:02 +0200

    auto datefield

--- Edu/news/forms.py
+++ Edu/news/forms.py
@@ -4,7 +4,7 @@ from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
 class ArticleFrom(ModelForm):
     class Meta:
         model = Article
-        fields = ['title', 'anons', 'full_text', 'date'] # date deleted
+        fields = ['title', 'anons', 'full_text'] # date deleted
 
         widgets = {
             'title' : TextInput(attrs={
@@ -19,8 +19,8 @@ class ArticleFrom(ModelForm):
                 'class' : 'form-control',
                 'placeholder' : "Текст статьи"
             }),
-            'date' : DateTimeInput(attrs={
-                'class' : 'form-control',
-                'placeholder' : 'Дата публикации'
-            })
+            # 'date' : DateTimeInput(attrs={
+            #     'class' : 'form-control',
+            #     'placeholder' : 'Дата публикации'
+            # })
         }
